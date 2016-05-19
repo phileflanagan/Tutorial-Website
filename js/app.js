24d6a182919a51bc632e2e11350fa5d104dbf7b1
Phil Flanagan <phileflanagan@gmail.com>
2016-05-18 22:34:30 -0400

    Overhaul structure and Implement Multiple Tutorial Option

--- js/app.js
+++ js/app.js
@@ -1,37 +1,64 @@
 // Module
-var howToApp = angular.module('howToApp', ['ngRoute', 'ngResource']);
+var howToApp = angular.module('howToApp', []);
 
-// Routes
-howToApp.config(function($routeProvider) {
-    console.log($routeProvider);
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home.html',
-            controller: 'howToCtrl',
-        })
-    
-        .when('/tutorial', {
-            templateUrl: 'pages/tutorial.html',
-            controller: 'tutCtrl',
-        })
-        
-        .when('/tutorial/:id', {
-            templateUrl: 'pages/tutorial.html',
-            controller: 'tutCtrl',
-        })
-        
-        .otherwise({
-            redirectTo: '/'
-        })
 
-    
-});
+// Factories and Services
+howToApp.factory('tutorials', ['$http', function ($http) {
+    return $http.get('json/tutorials.json')
+        .success(function (data) {
+            return data;
+        })
+        .error(function (err) {
+            return err;
+        });
+}]);
 
 // Controllers
-howToApp.controller('howToCtrl', ['$scope', function($scope) {
-    
+howToApp.controller('howToCtrl', ['$scope', 'tutorials', function ($scope, tutorials) {
+    $scope.currTutorialRef = $scope.currTutorialRef || 'firsttutorial';
+    $scope.currStep = $scope.currStep || '1';
+    $scope.tutList = {};
+
+    $scope.currTutorial = '';
+    console.log($scope.currTutorial);
+
+    tutorials.success(function (data) {
+        $scope.tutList = data;
+            console.log($scope.tutList);
+    });
+    //    function($scope, tutSvc) {
+    //        $scope.step = tutSvc.step;
+    //        $scope.$watch('step', function(){
+    //           tutSvc.step = $scope.step; 
+    //        });
+    //    }
 }]);
 
-howToApp.controller('tutCtrl', ['$scope', function($scope) {
-    
-}]);
+
+// Directives 
+howToApp.directive('tut', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            step: '@',
+            tutorial: '@'
+        },
+        link: function ($scope) {
+            $scope.$watch('step', function (step) {
+                if (step && step.length) {
+                    $scope.dynamicTemplateUrlStep1 = '/step' + step + '.html';
+                    $scope.dynamicTemplateUrl = $scope.dynamicTemplateUrlStep2 + $scope.dynamicTemplateUrlStep1;
+                }
+            });
+            $scope.$watch('tutorial', function (tutorial) {
+                if (tutorial && tutorial.length) {
+                    $scope.dynamicTemplateUrlStep2 = 'pages/' + tutorial;
+                    $scope.dynamicTemplateUrl = $scope.dynamicTemplateUrlStep2 + $scope.dynamicTemplateUrlStep1
+                }
+            });
+           
+        },
+
+        template: '<ng-include src="dynamicTemplateUrl"></ng-include>'
+    };
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-
-});
+//$(document).ready(function(){
+//    $('.list-group-item').click(function() {
+//        $(this).siblings('a').removeClass("active");
+//        $(this).addClass('active');
+//    })
+//});
